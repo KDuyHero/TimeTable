8bc7ff10a68dc30814f0c2c8d03da98afe3486c0
KDuyHero <dtrang1570@gmail.com>
2022-09-07 16:02:32 +0700

    Duy01
    dfasdfd

--- src/Object/Class.java
+++ src/Object/Class.java
@@ -6,13 +6,13 @@ package Object;
  */
 public class Class {
 
-    public String name;         // Tên học phần
-    public String id;              // Mã lớp
-    public String kindClass;   //  Loại lớp
-    public String kClass;       // Mã lớp kèm
-    public int time[][] = new int[12][7];   // Ma trận lưu thời gian của lớp
-    public String week;         // Tuần học
-    public String room;         // Phòng học
+    public String name; // Tên học phần
+    public String id; // Mã lớp
+    public String kindClass; // Loại lớp
+    public String kClass; // Mã lớp kèm
+    public int time[][] = new int[12][7]; // Ma trận lưu thời gian của lớp
+    public String week; // Tuần học
+    public String room; // Phòng học
 
     public Class(String id, String name, int[][] time, String kindClass, String kClass, String week) {
         this.id = id;
@@ -22,6 +22,7 @@ public class Class {
         this.kClass = kClass;
         this.week = week;
     }
+
     // Lấy ra loại lớp
     public String getKindClass() {
         return "(" + this.kindClass + ") " + this.name;
